3d0536480bd1dff387831b32bcd6fbb5719c338f
Yuxuan Ye <hs20307@it045310.wks.bris.ac.uk>
2025-02-12 02:01:43 +0000

    add postprocess to llm output

--- run-post_process.sh
+++ run-post_process.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 python post_process.py \
-    --csv_path completions/tldr/gpt-j-6b/test-greedy.csv \
-    --save_path completions/tldr/gpt-j-6b/test-greedy.csv
+    --csv_path completions/gpt-j-6b-tldr/greedy.csv \
+    --save_path completions/gpt-j-6b-tldr/greedy.csv
 
 python post_process.py \
-    --csv_path completions/tldr/gpt-j-6b/test-beam6-1.csv \
-    --save_path completions/tldr/gpt-j-6b/test-beam6-1.csv
+    --csv_path completions/gpt-j-6b-tldr/test-beam6-1.csv \
+    --save_path completions/gpt-j-6b-tldr/test-beam6-1.csv
 
 python post_process.py \
-    --csv_path completions/tldr/gpt-j-6b/test-beam6-2.csv \
-    --save_path completions/tldr/gpt-j-6b/test-beam6-2.csv
+    --csv_path completions/gpt-j-6b-tldr/test-beam6-2.csv \
+    --save_path completions/gpt-j-6b-tldr/test-beam6-2.csv
 
 python post_process.py \
-    --csv_path completions/tldr/gpt-j-6b/test-temp5-1.csv \
-    --save_path completions/tldr/gpt-j-6b/test-temp5-1.csv
+    --csv_path completions/gpt-j-6b-tldr/test-temp5-1.csv \
+    --save_path completions/gpt-j-6b-tldr/test-temp5-1.csv
 
 python post_process.py \
-    --csv_path completions/tldr/gpt-j-6b/test-temp5-2.csv \
-    --save_path completions/tldr/gpt-j-6b/test-temp5-2.csv
+    --csv_path completions/gpt-j-6b-tldr/test-temp5-2.csv \
+    --save_path completions/gpt-j-6b-tldr/test-temp5-2.csv
